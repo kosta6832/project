aeb101c12323944203766054f80767f646452486
Kostya <mail@mail.ru>
2020-07-23 10:27:26 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+'use strict';
+
+// console.log("arr" + " - arr2");
+// console.log(4 + " - arr2");
+
+let incr = 10,
+	decr = 10;
+
+console.log(incr++);
+console.log(decr--);
+
+console.log(5%2);
+console.log("2" === 2);
+
+let isC = true,
+	isClose = true;
+
+console.log(isC || isClose);
